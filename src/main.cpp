ca2ced4a4ab99e6c65892f1ed96b49a07bc500e1
T. Suzuki <st09d18@icloud.com>
2021-04-21 12:06:12 +0900

    Update ある程度修飾キー追加。

--- src/main.cpp
+++ src/main.cpp
@@ -35,13 +35,55 @@ void loop() {
             M5.Lcd.print((char)key_val);
           } else {
             M5.Lcd.printf("0x%02X ",key_val);
+            switch(key_val) {
+              case 0xAC:// Alt+$ -> ctrl+spaceに割り当て
+                M5.Lcd.printf("ctrl+space ");
+                bleKeyboard.press(KEY_LEFT_CTRL);
+                bleKeyboard.press(0x20);
+                delay(100);
+                bleKeyboard.releaseAll();
+                break;
+              case 0x0D:
+                M5.Lcd.printf("Return ");
+                bleKeyboard.write(KEY_RETURN);
+                break;
+              case 0x08:
+                M5.Lcd.printf("BackSpace ");
+                bleKeyboard.write(KEY_BACKSPACE);
+                break;
+              case 0x7F:
+                M5.Lcd.printf("Del ");
+                bleKeyboard.write(KEY_DELETE);
+                break;
+              case 0xBF:
+                M5.Lcd.printf("Left ");
+                bleKeyboard.write(KEY_LEFT_ARROW);
+                break;
+              case 0xC0:
+                M5.Lcd.printf("Down ");
+                bleKeyboard.write(KEY_DOWN_ARROW);
+                break;
+              case 0xC1:
+                M5.Lcd.printf("Right ");
+                bleKeyboard.write(KEY_RIGHT_ARROW);
+                break;
+              case 0xB7:
+                M5.Lcd.printf("Up ");
+                bleKeyboard.write(KEY_UP_ARROW);
+                break;
+              case 0xBA:
+                M5.Lcd.printf("Tab ");
+                bleKeyboard.write(KEY_TAB);
+                break;
+              default:
+                break;
+            }
           }
         }
       }
     }
     //Serial.println("Sending Enter key...");
     //bleKeyboard.print("Hello world");
-    //bleKeyboard.write(KEY_RETURN);
 
     //Serial.println("Sending Ctrl+Alt+Delete...");
     //bleKeyboard.press(KEY_LEFT_CTRL);
